ee6f1738ec9edb8e829856454f5914676a0870e3
Shir <shchorev@gmail.com>
2018-01-28 01:37:08 +0200

    added option to compile model with wanted parameters

--- keras_retinanet/bin/compile.py
+++ keras_retinanet/bin/compile.py
@@ -13,22 +13,37 @@ from .. import losses
 from ..models.resnet import custom_objects
 
 
-def compile_model(model_path):
+def compile_model(model_path, alpha=0.25, gamma=2.0, output_path="", lr=1e-5):
     print('Loading model, this may take a second...')
     model = keras.models.load_model(model_path, custom_objects=custom_objects)
     # compile model
     model.compile( 
             loss={  'regression'    : losses.smooth_l1(),        
-                    'classification': losses.focal()         },
-                    optimizer=keras.optimizers.adam(lr=1e-5, clipnorm=0.001)     
+                    'classification': losses.focal(alpha=alpha, gamma=gamma)},
+                    optimizer=keras.optimizers.adam(lr=lr, clipnorm=0.001)     
             )
-    model.save(model_path)
+    if output_path == "":
+        output_path = model_path
 
-def main():
-    # print command line arguments
-    for arg in sys.argv[1:]:
-        print("compiling model {}".format(arg))
-        compile_model(arg)
-            
+    model.save(output_path)
+
+def parse_args(args):
+    parser     = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
+    parser.add_argument('--gamma', help='Gamma value for focal loss', type=float, default=2.0)
+    parser.add_argument('--alpha', help='Alpha value for focal loss', type=float, default=0.25)
+    parser.add_argument('--learning-rate', help='Initial learning rate for adam',  type=float, dest='lr', default=1e-5)
+    parser.add_argument('--output-path', help='path for saving compiled model', default="")
+    parser.add_argument('model_path', help='Model to compile')
+    return check_args(parser.parse_args(args))
+
+def main(args=None):
+    # parse arguments
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+
+    print("compiling model {} with gamma {} and alpha {}".format(args.model_path, args.alpha, args.gamma))
+    compile_model(args.model_path, args.alpha, args.gamma, args.output_path, args.lr)
+
 if __name__ == "__main__":
     main()
